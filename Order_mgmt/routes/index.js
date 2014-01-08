dbecaea964d6618e92aab132ce8baef28b8d731d
Raj <rajkum@thoughtworks.com>
2014-01-08 22:01:50 +0530

    One simple order being inserted

--- Order_mgmt/routes/index.js
+++ Order_mgmt/routes/index.js
@@ -9,9 +9,10 @@ var viewQueryResults = function(query, res, perFormOperation){
 	connection.end();
 }
 
-var executeQuery = function(query, dataToInsert, res, insertFunction){
+var executeQuery = function(query, dataToInsert, products, res, insertFunction){
 	var connection = mysql.createConnection(connectionObject);
-	connection.query(query,dataToInsert, insertFunction(res));
+	console.log("",dataToInsert);
+	connection.query(query,dataToInsert, insertFunction(res,products));
 	connection.end();
 }
 
@@ -25,28 +26,45 @@ exports.order = function(req, res){
 	var productsQuery = "select p_id,p_name,p_price from Product";
 	viewQueryResults(productsQuery,res,viewProducts);
 }
-var insertOrder = function(res){
+var order_items = function(products){
+	return function(err, result){
+		if (err) throw err;
+		console.log("yaha",products);
+		var connection = mysql.createConnection(connectionObject);
+		// var keys = Object.keys(products);
+		console.log(products);
+		connection.query("insert into order_items values("+result.insertId+",'rice_basmati',4 )",{},function(err,result){
+		if (err) throw err;
+
+		});
+	}
+}
+
+var insertOrder = function(res, products){
 	return function(err, result) {
 		 if (err) throw err;
 		 var query = "insert into orders (cust_id,amount)values("+result.insertId+",500)";
 		 var connection = mysql.createConnection(connectionObject);
-		 connection.query(query,{},function(err, result){
-		 	if (err) throw err;
-		 	console.log(result);
-		 });
+		 connection.query(query,{},order_items(products));
+		 	// console.log(result);
 	}
 	res.end();
 }
 exports.placeOrder = function(req, res){
 	customer = {};
 	products = req.body;
+	var actualProducts = {};
 	customer.name = products.user_name;
 	customer.contact_no = products.contact_no;
 	delete(products["user_name"]);
 	delete(products["contact_no"]);
+	var keys = Object.keys(products);
+	keys.forEach(function(item){
+		if(products[item] != ''){
+			actualProducts[item] = products[item][0]; 
+		}
+	});
 	console.log(customer);
-	console.log(products);
-
-	executeQuery('insert into customer set ?',customer,res,insertOrder);
-	// executeQuery('insert into orders set ?c',customer,res);
+	console.log(actualProducts);
+	executeQuery('insert into customer set ?',customer,actualProducts,res,insertOrder);
 }
